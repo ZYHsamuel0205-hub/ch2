f8552bc049d2a26eb1ceb432da832053803c246d
ZYHsamuel0205-hub <chongsamuel0205@gmail.com>
2026-09-14 13:26:56 -0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,23 @@
+public class data {
+
+	public static void main (String[] arg) {
+	//variable declaration
+	String day = "Monday";
+	String month = "September";
+	int date = 14;
+	int year = 2026;
+
+	System.out.print("American format: ");
+	System.out.print(day + ", ");
+	System.out.print(month + " ");
+	System.out.print(date + ", ");
+	System.out.println(year);
+	
+	System.out.print("Eurpean format: ");
+	System.out.print(day + " ");
+	System.out.print(month + " ");
+	System.out.print(date + " ");
+	System.out.println(year);
+	}
+
+}
