41e30232945a9084aa8630331526fe435844128d
ZYHsamuel0205-hub <chongsamuel0205@gmail.com>
2026-09-15 13:12:56 -0700

    Add files via upload

--- time.java
+++ time.java
@@ -1,9 +1,29 @@
 public class time {
 
 	public static void main (String[] arg) {
-	int hour = 13;
-	int minute = 30;
-	int second = ;
+	int hour = 12;
+	int minute = 20;
+	int second = 34;
+	int hourCurrent = 12;
+	int minuteCurrent = 50;
+	int secondCurrent = 23;
+	int timePassed = hour * 3600 + minute * 60 + second;
+	int timeRemaining = 86400 - hour * 3600 - minute * 60;
+	double percent = timePassed / 86400.0 * 100;
+	int timePassedSinceStarted = (hourCurrent * 3600 + minuteCurrent * 60 + secondCurrent) - timePassed;
+	
+	System.out.print("Current time: ");
+	System.out.print(hourCurrent + "hr: ");
+	System.out.print(minuteCurrent + "min: ");
+	System.out.println(secondCurrent + "sec");
+	
+	System.out.println("Number of seconds since midnight: " + timePassed + " seconds");
+	
+	System.out.println("Number of seconds remaining in the day: " + timeRemaining + " seconds");
+	
+	System.out.println("Percentage of the day that has passed: " + percent + "%");
+
+	System.out.println("Time passed since I started working on this exercise: " + timePassedSinceStarted + " seconds");
 	}
 
 }
